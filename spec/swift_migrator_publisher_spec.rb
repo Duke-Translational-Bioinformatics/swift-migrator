038209ee535ad22c9d49ca3108434c239dbc13f4
Darin London <darin.london@duke.edu>
2019-09-17 14:44:33 -0400

    publishes object definitions in csv file to amqp exchanges defined by is_multipart_upload

---
+++ spec/swift_migrator_publisher_spec.rb
@@ -0,0 +1,95 @@
+require_relative '../swift_migrator_publisher'
+require 'bunny-mock'
+require 'json'
+
+describe SwiftMigratorPublisher do
+  let(:mocked_bunny) { BunnyMock.new }
+  let(:task_queue_prefix) { 'test' }
+
+  subject {
+    SwiftMigratorPublisher.new mocked_bunny
+  }
+
+  it { is_expected.to respond_to(:publish_object).with(3).argument }
+  it { is_expected.to respond_to(:publish_objects_from).with(1).argument }
+
+  describe '#publish_object' do
+    let(:container_to_publish) { 'container-to-publish' }
+    let(:object_to_publish) { 'object-to-publish' }
+    let(:expected_message) {
+      JSON.dump({
+        container: container_to_publish,
+        object: object_to_publish,
+        is_multipart_upload: is_multipart_upload
+      })
+    }
+
+    before do
+      ENV['TASK_QUEUE_PREFIX'] = task_queue_prefix
+    end
+
+    context 'is_multipart_upload false' do
+      let(:is_multipart_upload) { false }
+      let(:task_queue) {
+        subject.channel.queue("#{ENV['TASK_QUEUE_PREFIX']}.single", durable: true)
+      }
+
+      it 'should publish the container, object, and is_multipart_upload as json to the single task queue' do
+        expect(task_queue).to be
+        # the bunny-mock default exchange is a standard BunnyMock::Exchange::Direct
+        # so a BunnyMock::Queue has to be manually bound to that exchange
+        # before running the test
+        task_queue.bind subject.exchange, routing_key: task_queue.name
+        subject.publish_object container_to_publish, object_to_publish, is_multipart_upload
+        expect(task_queue.message_count).to eq(1)
+        payload = task_queue.all.first
+        expect(payload[:message]).to eq(expected_message)
+      end
+    end
+
+    context 'is_multipart_upload true' do
+      let(:is_multipart_upload) { true }
+      let(:task_queue) {
+        subject.channel.queue("#{ENV['TASK_QUEUE_PREFIX']}.multipart", durable: true)
+      }
+
+      it 'should publish the container, object, and is_multipart_upload as json to the multipart task queue' do
+        expect(task_queue).to be
+        # the bunny-mock default exchange is a standard BunnyMock::Exchange::Direct
+        # so a BunnyMock::Queue has to be manually bound to that exchange
+        # before running the test
+        task_queue.bind subject.exchange, routing_key: task_queue.name
+        subject.publish_object container_to_publish, object_to_publish, is_multipart_upload
+        expect(task_queue.message_count).to eq(1)
+        payload = task_queue.all.first
+        expect(payload[:message]).to eq(expected_message)
+      end
+    end
+  end
+
+  describe '#publish_objects_from' do
+    let(:object_inputs) { [
+        'container-1,id-1,true',
+        'container-2,id-2,false'
+    ] }
+    let(:io_to_publish) {
+      io = StringIO.new("")
+      object_inputs.each do |object_input|
+        io.puts object_input
+      end
+      io.rewind
+      io
+    }
+
+    before do
+      object_inputs.each do |object_input|
+        this_container, this_object, this_is_multipart_upload = object_input.split(',')
+        is_expected.to receive(:publish_object)
+          .with(this_container, this_object, this_is_multipart_upload)
+      end
+    end
+    it 'should publish all input' do
+      subject.publish_objects_from io_to_publish
+    end
+  end
+end
---
+++ swift_migrator_publisher.rb
@@ -0,0 +1,67 @@
+#!/usr/local/bin/ruby
+
+class SwiftMigratorPublisher
+  require "bunny"
+  require 'json'
+
+  attr_accessor :connection, :channel, :exchange
+  def initialize(connection=nil)
+    if connection
+      @connection = connection
+    end
+    connect
+  end
+
+  def publish_object(container, object, is_multipart_upload)
+    queue_name = ''
+    if is_multipart_upload
+      queue_name = "#{ENV['TASK_QUEUE_PREFIX']}.multipart"
+    else
+      queue_name = "#{ENV['TASK_QUEUE_PREFIX']}.single"
+    end
+    message = JSON.dump({
+      container: container,
+      object: object,
+      is_multipart_upload: is_multipart_upload
+    })
+    @exchange.publish(message, routing_key: queue_name)
+  end
+
+  def publish_objects_from(io)
+    while (object_input = io.gets)
+      this_container, this_object, this_is_multipart_upload = object_input.chomp.split(',')
+      publish_object(this_container, this_object, this_is_multipart_upload)
+    end
+  end
+
+  private
+  def connect
+    unless @connection
+      @connection = Bunny.new(ENV['AMQP_URL'], automatically_recover: false)
+    end
+    @connection.start
+    @channel = @connection.create_channel
+    @exchange = @channel.default_exchange
+  end
+end
+
+def usage
+  $stderr.puts "usage: swift_migrator_publisher <path_to_file>
+  file must have lines with comma separated values container:object:is_multipart_upload
+  where is_multipart_upload must be either true or false
+
+  requires the following Environment Variables
+    AMQP_URL: full url to amqp service
+    TASK_QUEUE_PREFIX: prefix for names of queues used by the swift migrator subscribers
+  "
+  exit(1)
+end
+
+if $0 == __FILE__
+  input_file = ARGV.shift or die usage
+  die usage unless(ENV['AMQP_URL'] && ENV['TASK_QUEUE_PREFIX'])
+  File.open(input_file, 'r') do |object_input_io|
+    SwiftMigratorPublisher.new.publish_objects_from object_input_io
+  end
+  $stderr.puts "all published"
+end
